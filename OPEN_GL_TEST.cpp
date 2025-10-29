e1fa4704c91c51a55a7c55925c14f1de6466eb9c
Patrick Pleumann <patrickpleumann@web.de>
2025-10-29 16:58:04 +0100

    initial

---
+++ OPEN_GL_TEST.cpp
@@ -0,0 +1,55 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+#include "glm/glm.hpp"
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+
+	window = glfwCreateWindow(800, 800, "Test", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		glfwTerminate();
+		return -2;
+	}
+
+	float vertices[] = //triangle
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f, 0.5f, 0.0f
+	};
+
+
+	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
